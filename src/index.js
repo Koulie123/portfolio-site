82c9974c5e08288bfe66004dac5826e6a82e84a5
Koulie <kouliekyriakos@hotmail.com>
2024-06-19 18:15:31 -0400

    Added pictre of me and div which will be the about me as well as some styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style.css';
+import PictureOfMe from './images/SmallPicOfMe.jpg';
+
+
+
+const setPicture = function () {
+    const picDiv = document.querySelector('.pic-of-me');
+    const img = new Image();
+    img.src = PictureOfMe;
+
+    picDiv.appendChild(img);
+}
+
+
+setPicture();
